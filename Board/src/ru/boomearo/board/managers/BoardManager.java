0d1ba19bab8a87e3b2e82369fd915efd140c3cdd
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2020-08-18 19:50:04 +0300

    use concurrentmap and minor thread saves

--- Board/src/ru/boomearo/board/managers/BoardManager.java
+++ Board/src/ru/boomearo/board/managers/BoardManager.java
@@ -2,11 +2,11 @@ package ru.boomearo.board.managers;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.bukkit.ChatColor;
 
@@ -15,64 +15,74 @@ import ru.boomearo.board.objects.boards.AbstractBoard;
 
 public final class BoardManager {
 
-	public static final String prefix = "§8[§6Board§8]: §f";
-	
-	private final Map<String, PlayerBoard> playerBoards = new HashMap<String, PlayerBoard>();
-	
-	private final Set<String> playersIgnore = new HashSet<String>();
-	
-	private AbstractBoard board = null;
-	
+    public static final String prefix = "§8[§6Board§8]: §f";
+
+    private final ConcurrentMap<String, PlayerBoard> playerBoards = new ConcurrentHashMap<String, PlayerBoard>();
+
+    private final Set<String> playersIgnore = new HashSet<String>();
+
+    private AbstractBoard board = null;
+
+    private final Object lock = new Object();
+
     private static List<String> entryNames = new ArrayList<String>();
     static {
-    	//Инициализируем сразу же при создании класса названия
+        //Инициализируем сразу же при создании класса названия
         for (ChatColor color : ChatColor.values()) {
             entryNames.add("" + color + ChatColor.RESET);
         }
     }
-	
-	public PlayerBoard getPlayerBoard(String player) {
-		return this.playerBoards.get(player);
-	}
-	public void addPlayerBoard(PlayerBoard board) {
-		this.playerBoards.put(board.getPlayer().getName(), board);
-	}
-	public void removePlayerBoard(String player) {
-		PlayerBoard pb = this.playerBoards.get(player);
-		if (pb != null) {
-			this.playerBoards.remove(player);
-			pb.remove();
-		}
-	}
-	public Collection<PlayerBoard> getAllPlayerBoards() {
-		return this.playerBoards.values();
-	}
-	
-	public AbstractBoard getBoard() {
-		return this.board;
-	}
-	
-	public void setBoard(AbstractBoard board) {
-		this.board = board;
-	}
-	
-	public boolean isIgnore(String player) {
-		return this.playersIgnore.contains(player);
-	}
-	
-	public void addIgnore(String player) {
-		this.playersIgnore.add(player);
-	}
-	public void removeIgnore(String player) {
-		this.playersIgnore.remove(player);
-	}
-	
-	public List<String> getAllIgnores() {
-		return new ArrayList<String>(this.playersIgnore);
-	}
-	
-	public static String getColor(int index) {
-		return entryNames.get(index);
-	}
-	
+
+    public PlayerBoard getPlayerBoard(String player) {
+        return this.playerBoards.get(player);
+    }
+    public void addPlayerBoard(PlayerBoard board) {
+        this.playerBoards.put(board.getPlayer().getName(), board);
+    }
+    public void removePlayerBoard(String player) {
+        PlayerBoard pb = this.playerBoards.get(player);
+        if (pb != null) {
+            this.playerBoards.remove(player);
+            pb.remove();
+        }
+    }
+    public Collection<PlayerBoard> getAllPlayerBoards() {
+        return this.playerBoards.values();
+    }
+
+    public AbstractBoard getBoard() {
+        return this.board;
+    }
+
+    public void setBoard(AbstractBoard board) {
+        this.board = board;
+    }
+
+    public boolean isIgnore(String player) {
+        synchronized (this.lock) {
+            return this.playersIgnore.contains(player);
+        }
+    }
+
+    public void addIgnore(String player) {
+        synchronized (this.lock) {
+            this.playersIgnore.add(player);
+        }
+    }
+    public void removeIgnore(String player) {
+        synchronized (this.lock) {
+            this.playersIgnore.remove(player);
+        }
+    }
+
+    public Set<String> getAllIgnores() {
+        synchronized (this.lock) {
+            return new HashSet<String>(this.playersIgnore);
+        }
+    }
+
+    public static String getColor(int index) {
+        return entryNames.get(index);
+    }
+
 }
