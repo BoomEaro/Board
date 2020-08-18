676f3df1eeb3d467f5525fcc9c1fff992f766723
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2020-08-18 19:09:15 +0300

    add thread save and add throws exceptions

---
+++ Board/src/ru/boomearo/board/exceptions/BoardException.java
@@ -0,0 +1,11 @@
+package ru.boomearo.board.exceptions;
+
+public class BoardException extends Exception {
+
+    private static final long serialVersionUID = 2360252814722231136L;
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+    
+}
--- Board/src/ru/boomearo/board/objects/PlayerBoard.java
+++ Board/src/ru/boomearo/board/objects/PlayerBoard.java
@@ -9,103 +9,121 @@ import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.*;
 
 import ru.boomearo.board.Board;
+import ru.boomearo.board.exceptions.BoardException;
 import ru.boomearo.board.managers.BoardManager;
 import ru.boomearo.board.objects.boards.AbstractPage;
 import ru.boomearo.board.objects.boards.AbstractHolder;
 
 public class PlayerBoard {
 
-	private Player player;
-	
-	private Scoreboard scoreboard;
-	private Objective objective;
-    
+    private final Player player;
+
+    private Scoreboard scoreboard;
+    private Objective objective;
+
     private List<AbstractPage> pages = null;
     private final List<TeamInfo> teams = new ArrayList<TeamInfo>();
-	private int pageIndex = 0;
+    private volatile int pageIndex = 0;
 
     private volatile boolean cd = false;
-	
-	private int updatePageCount = 0;
-	private boolean permanentView = false;
-	private boolean debugMode = false;
 
-	
-	public PlayerBoard(Player player) {
-		this.player = player;
-		
-		Objective ob = player.getScoreboard().getObjective(DisplaySlot.SIDEBAR);
-		if (ob != null) {
+    private volatile int updatePageCount = 0;
+    private boolean permanentView = false;
+    private boolean debugMode = false;
+
+    private final Object lock = new Object();
+
+    public PlayerBoard(Player player) {
+        this.player = player;
+        
+        removeBoardIfExists();
+        
+        //Инициализируем панель
+        try {
+            buildScoreboard();
+            //Получаем новые экземплярпы страниц для игрока
+            initPages();
+            //Устанавливаем страницу
+            setUpPage(getCurrentPage());
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    private void removeBoardIfExists() {
+        Objective ob = this.player.getScoreboard().getObjective(DisplaySlot.SIDEBAR);
+        if (ob != null) {
             ob.unregister();
-			player.getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
-		}
-		//Инициализируем панель
-        buildScoreboard();
-		//Получаем новые экземплярпы страниц для игрока
-        initPages();
-		//Устанавливаем страницу
-		setUpPage(getCurrentPage());
-		
-	}
-	
-	public void initPages() {
-		this.pageIndex = 0;
-		
-		this.pages = Board.getContext().getBoardManager().getBoard().getPages(this);
-	}
-	
-	public AbstractPage getCurrentPage() {
-		return this.pages.get(this.pageIndex);
-	}
-	
-	public List<AbstractPage> getPages() {
-		return this.pages;
-	}
-	
-	public AbstractPage getPageByIndex(int index) {
-		
-		if (index < 0) {
-			return null;
-		}
-		
-		if (index > getMaxPageIndex()) {
-			return null;
-		}
-		
-		return this.pages.get(index);
-	}
-	
-	public int getMaxPageIndex() {
-		return this.pages.size() - 1;
-	}
-	
-    public int getNextPageNumber() {
-		int maxPage = getMaxPageIndex();
-		for (int i = this.pageIndex; i <= maxPage; i++) {
-			int next = i + 1;
-			if (next > maxPage) {
-				return 0;
-			}
-			AbstractPage nextPage = this.pages.get(next);
-			if (nextPage.isVisible()) {
-				return next;
-			}
-		}
-		return 0;
-		
-	}
-	
-	public int getUpdatePageCount() {
-		return this.updatePageCount;
-	}
-	public void addUpdatePageCount(int time) {
-		this.updatePageCount = this.updatePageCount + time;
-	}
-	public void setUpdatePageCount(int time) {
-		this.updatePageCount = time;
-	}
+            this.player.getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
+        }
+    }
 
-    private void buildScoreboard() {
+    public void initPages() {
+        this.pageIndex = 0;
+
+        this.pages = Board.getContext().getBoardManager().getBoard().getPages(this);
+    }
+
+    public AbstractPage getCurrentPage() {
+        synchronized (this.lock) {
+            return this.pages.get(this.pageIndex);
+        }
+    }
+
+    public AbstractPage getPageByIndex(int index) {
+        synchronized (this.lock) {
+            if (index < 0) {
+                return null;
+            }
+
+            if (index > getMaxPageIndex()) {
+                return null;
+            }
+
+            return this.pages.get(index);
+        }
+    }
+
+    public int getMaxPageIndex() {
+        synchronized (this.lock) {
+            return this.pages.size() - 1;
+        }
+    }
+
+    public int getNextPageNumber() {
+        synchronized (this.lock) {
+            int maxPage = getMaxPageIndex();
+            for (int i = this.pageIndex; i <= maxPage; i++) {
+                int next = i + 1;
+                if (next > maxPage) {
+                    return 0;
+                }
+                AbstractPage nextPage = this.pages.get(next);
+                if (nextPage.isVisible()) {
+                    return next;
+                }
+            }
+            return 0;
+        }	
+    }
+
+    public int getUpdatePageCount() {
+        return this.updatePageCount;
+    }
+    public void addUpdatePageCount(int time) {
+        this.updatePageCount = this.updatePageCount + time;
+    }
+    public void setUpdatePageCount(int time) {
+        this.updatePageCount = time;
+    }
+
+    private void buildScoreboard() throws BoardException {
+        if (!Bukkit.isPrimaryThread()) {
+            throw new BoardException("ScoreBoard должен быть создан в основном потоке!");
+        }
+        
         this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
         this.objective = this.scoreboard.registerNewObjective("Board", "dummy");
         this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
@@ -114,51 +132,58 @@ public class PlayerBoard {
     }
 
     private void setUpPage(AbstractPage page) {
-    	//Устанавливаем страницу и заполняем лист тимами с плейсхолдерами
-    	int index = 15;
-    	
-    	this.objective.setDisplayName(page.getTitle());
-    	
-    	for (AbstractHolder holder : page.getReadyHolders()) {
-            Team team = this.scoreboard.registerNewTeam("Team:" + index);
-            String sc = BoardManager.getColor(index);
-            team.addEntry(sc);
-    		String[] text = holder.getResult();
-            setPrefix(team, text[0]);
-            setSuffix(team, text[1]);
-            this.objective.getScore(sc).setScore(index);
-            this.teams.add(new TeamInfo(team, holder));
-            index--;
-    	}
+        synchronized (this.lock) {
+            //Устанавливаем страницу и заполняем лист тимами с плейсхолдерами
+            int index = 15;
+
+            this.objective.setDisplayName(page.getTitle());
+
+            for (AbstractHolder holder : page.getReadyHolders()) {
+                Team team = this.scoreboard.registerNewTeam("Team:" + index);
+                String sc = BoardManager.getColor(index);
+                team.addEntry(sc);
+                String[] text = holder.getResult();
+                setPrefix(team, text[0]);
+                setSuffix(team, text[1]);
+                this.objective.getScore(sc).setScore(index);
+                this.teams.add(new TeamInfo(team, holder));
+                index--;
+            }
+        }
     }
 
-    
+
     public void toPage(int indexTo, AbstractPage toPage) {
-		this.updatePageCount = 0;
-		this.pageIndex = indexTo;
-		loadPage(toPage);
-		
-		update();
+        synchronized (this.lock) {
+            this.updatePageCount = 0;
+            this.pageIndex = indexTo;
+
+            loadPage(toPage);
+
+            update();
+        }
     }
 
     public void update() {
-        if (this.cd) {
-            return;
-        }
-        //Обновляем инфу согласно кастомным холдерам
-        for (TeamInfo team : this.teams) {
-        	//long start = System.nanoTime();
-        	String[] text = team.getHolder().getResult();
-        	//long end = System.nanoTime();
-        	//Board.getContext().getLogger().info("'" + text[0] + text[1] + "' -- " +(end - start));
-            
-            //Board.getContext().getLogger().info("test " + test[0] + "§r<->" + test[1]);
-            setPrefix(team.getTeam(), text[0]);
+        synchronized (this.lock) {
+            if (this.cd) {
+                return;
+            }
+            //Обновляем инфу согласно кастомным холдерам
+            for (TeamInfo team : this.teams) {
+                //long start = System.nanoTime();
+                String[] text = team.getHolder().getResult();
+                //long end = System.nanoTime();
+                //Board.getContext().getLogger().info("'" + text[0] + text[1] + "' -- " +(end - start));
 
-            setSuffix(team.getTeam(), text[1]);
-            
-            //Board.getContext().getLogger().info("test " + test[0] + " " + test[1]);
-        }    
+                //Board.getContext().getLogger().info("test " + test[0] + "§r<->" + test[1]);
+                setPrefix(team.getTeam(), text[0]);
+
+                setSuffix(team.getTeam(), text[1]);
+
+                //Board.getContext().getLogger().info("test " + test[0] + " " + test[1]);
+            }   
+        } 
     }
 
     private void setPrefix(Team team, String text) {
@@ -170,20 +195,30 @@ public class PlayerBoard {
     }
 
     public void remove() {
-        this.player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+        synchronized (this.lock) {
+            if (Bukkit.isPrimaryThread()) {
+                this.player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+            }
+            else {
+                Bukkit.getScheduler().runTask(Board.getContext(), () -> {
+                    this.player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+                });
+            }
+        }
     }
 
     private void loadPage(AbstractPage page) {
-        this.cd = true;
-        removeAll();
+        synchronized (this.lock) {
+            this.cd = true;
+            removeAll();
 
-        setUpPage(page);
+            setUpPage(page);
 
-        this.cd = false;
+            this.cd = false;
+        }
     }
 
     private void removeAll() {
-        
         Set<Team> teams = this.scoreboard.getTeams();
         if (teams != null) {
             for (Team t : teams) {
@@ -199,7 +234,6 @@ public class PlayerBoard {
         }
 
         this.teams.clear();
-
     }
 
 
@@ -214,24 +248,24 @@ public class PlayerBoard {
     public Player getPlayer() {
         return this.player;
     }
-  
-    
+
+
     public int getPageIndex() {
-    	return this.pageIndex;
+        return this.pageIndex;
     }
     public void setPageIndex(int index) {
-    	this.pageIndex = index;
+        this.pageIndex = index;
     }
     public void setPermanentView(boolean view) {
-    	this.permanentView = view;
+        this.permanentView = view;
     }
     public boolean isPermanentView() {
-    	return this.permanentView;
+        return this.permanentView;
     }
     public void setDebugMode(boolean mode) {
-    	this.debugMode = mode;
+        this.debugMode = mode;
     }
     public boolean isDebugMode() {
-    	return this.debugMode;
+        return this.debugMode;
     }
 }
