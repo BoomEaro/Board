fcf079bc687102c57337dd1413c79161ea41cff3
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-08-08 20:56:59 +0300

    Add support to 1.19

--- src/main/java/ru/boomearo/board/utils/StringUtils.java
+++ src/main/java/ru/boomearo/board/utils/StringUtils.java
@@ -21,7 +21,7 @@ public final class StringUtils {
             case "1_17_R1": {
                 return 64;
             }
-            case "1_18_R2": {
+            case "1_19_R1": {
                 return Integer.MAX_VALUE;
             }
             default: {
