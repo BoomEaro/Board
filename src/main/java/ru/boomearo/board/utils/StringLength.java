62ad11b326cef99f55acc4d7cbfc5537bbc2d11d
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2023-07-02 21:45:30 +0300

    Support 1.20.1

--- src/main/java/ru/boomearo/board/utils/StringLength.java
+++ src/main/java/ru/boomearo/board/utils/StringLength.java
@@ -18,7 +18,10 @@ public final class StringLength {
     private static DataLength calculateLength(String version) {
         switch (version) {
             case "1_16_R3":
-            case "1_17_R1": {
+            case "1_17_R1":
+            case "1_18_R1":
+            case "1_19_R3":
+            case "1_20_R1": {
                 return new DataLength(64, 128);
             }
             default: {
